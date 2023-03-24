e34cd560089bfcd9fcd5aa5fd7da3fc25a71803c
caugusto <caugusto_ny@yahoo.com>
2023-03-24 13:50:05 -0400

    New Updated

--- variables.tf
+++ variables.tf
@@ -1,25 +1,63 @@
-/**
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+# --------------------------------------------------
+# VARIABLES
+# Set these before applying the configuration
+# --------------------------------------------------
 
 variable "project_id" {
-  description = "The project ID to deploy to"
   type        = string
+  description = "Google Cloud Project ID"
 }
 
 variable "bucket_name" {
-  description = "The name of the bucket to create"
   type        = string
+  description = "Bucket where source data is stored"
+  default     = "data-analytics-demos"
+}
+variable "region" {
+  type        = string
+  description = "Google Cloud Region"
+  default     = "us-central1"
+}
+
+variable "dataset_id" {
+  type        = string
+  description = "Google Cloud BQ Dataset ID"
+  default     = "gcp"
+}
+
+
+variable "labels" {
+  type        = map(string)
+  description = "A map of labels to apply to contained resources."
+  default     = { "edw-bigquery" = true }
+}
+
+variable "enable_apis" {
+  type        = string
+  description = "Whether or not to enable underlying apis in this solution. ."
+  default     = true
+}
+
+variable "force_destroy" {
+  type        = string
+  description = "Whether or not to protect BigQuery resources from deletion when solution is modified or changed."
+  default     = true
+}
+
+variable "deletion_protection" {
+  type        = string
+  description = "Whether or not to protect GCS resources from deletion when solution is modified or changed."
+  default     = true
+}
+
+variable "use_case_short" {
+  type        = string
+  description = "Short name for use case"
+  default     = "lakehouse"
+}
+
+variable "public_data_bucket" {
+  type        = string
+  description = "Public Data bucket for access"
+  default     = "data-analytics-demos"
 }
